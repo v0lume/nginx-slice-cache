a19e5b68b0b06f4f9a9d1fca4e81449adcd5ca0b
Artsiom Shmanouski <hav3y0u@gmail.com>
2022-01-20 01:25:45 +0300

    init nodejs implementation

---
+++ routes/range.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var router = express.Router();
+
+router.get('/nodejs/:filename', function(req, res, next) {
+  var file = path.resolve(__dirname, '../public/videos/', req.params.filename);
+    fs.stat(file, function(err, stats) {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          // 404 Error if file not found
+          return res.sendStatus(404);
+        }
+
+        res.end(err);
+      }
+
+      var range = req.headers.range;
+
+      if (!range) {
+       // 416 Wrong range
+       return res.sendStatus(416);
+      }
+      var positions = range.replace(/bytes=/, "").split("-");
+      var start = parseInt(positions[0], 10);
+      var total = stats.size;
+      var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+      var chunksize = (end - start) + 1;
+
+      res.writeHead(206, {
+        "Content-Range": "bytes " + start + "-" + end + "/" + total,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4"
+      });
+
+      var stream = fs.createReadStream(file, { start: start, end: end })
+        .on("open", function() {
+          stream.pipe(res);
+        }).on("error", function(err) {
+          res.end(err);
+        });
+    });
+});
+
+module.exports = router;
